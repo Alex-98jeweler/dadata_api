fa18cdd42bbccf04a38c51ec83a0c94068c1eb4e
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-02-08 10:43:24 +0700

    any changes dadata_client

--- dadata_client.py
+++ dadata_client.py
@@ -20,23 +20,29 @@ def event_loop(client, language):
                 break
             res = client.suggest('address', query)
             if len(res) > 0:
-                print("Есть ли необходимое значение в списке?\n")
+                print("Есть ли необходимое значение в списке?")
                 for i in range(len(res)):
                     print(f"{i + 1}. {res[i]['value']}")
 
-                number = input("0 - нет и ввести запрос заново, номер дома, номер квартиры значения - да\n>>> ")
-
+                number = input("0 - нет и ввести запрос заново, номер значения - да\n>>> ")
+                print('\n')
                 try:
                     number = int(number)
                 except ValueError as error:
                     if len(res) > 1:
                         print(f"Ошибка! Введите число от 1 до {len(res)}")
+                        number = input(">>> ")
+                        number = int(number)
                     else:
                         print("Введите цифру 1")
-
+                        number = input(">>> ")
+                        number = int(number)
                 if number == 0:
                     query = input("Введите запрос(город, улица, номер дома, номер квартиры):\n>>> ")
                 else:
+                    if number < 0 or number > len(res):
+                        print(f"Ошибка! Число находится вне допустимого диапазона! Введите число от 1 до {len(res)}")
+                        number = int(input(">>>"))
                     query = res[number - 1]['value']
                     flag = 0
             else:
@@ -45,10 +51,12 @@ def event_loop(client, language):
         result = client.clean('address', query)
         print(f"Адрес: {result['result']}, Долгота: {result['geo_lon']}, широта: {result['geo_lat']}")
         repeat = input("Продолжим?(да/нет)\n>>> ")
-        if repeat == 'да':
-            query = input("Введите запрос(город, улица, номер дома, номер квартиры | off - выйти из клиента):\n>>> ")
-        else:
+        if repeat == 'нет':
             mode = 0
+        else:
+            query = input("Введите запрос(формат - город, улица, номер дома, номер квартиры | 'off' - выйти из клиента) :\n>>> ")
+            flag = 1
+
 
 
 
